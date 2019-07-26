c80c5cc39d44b1e0de93d80d6d69408d0f268ca7
AgeevSashka <40717635+AgeevSashka@users.noreply.github.com>
2019-07-26 11:39:02 +0300

    Add files via upload

---
+++ Dz_2/src/functions.php
@@ -0,0 +1,75 @@
+<?php
+//function task1($sec){
+//    $firsts = ['Hi ! ' , 'How ', 'are ', 'you?'];
+//    foreach ($firsts as $first) {
+//        echo '<p>' . $first . '</p>';
+//    }
+//    if($sec === true) {
+//        foreach ($firsts as $first) {
+//            echo $first;
+//        }
+//    } else {
+//        return false;
+//    }
+//}
+//task1();
+
+
+/*function task2($str1, $str2, $str3, $str4)
+{
+    if($str1 == '+') {
+        echo $str2 + $str3 + $str4;
+    } elseif ($str1 == '-') {
+        echo $str2 - $str3 - $str4;
+    } elseif ($str1 == '*') {
+        echo $str2 * $str3 * $str4;
+    } elseif ($str1 == '/') {
+        echo $str2 / $str3 / $str4;
+    } elseif ($str1 == '%') {
+        echo $str2 % $str3 % $str4;
+    } else {
+        echo 'ERROR';
+    }
+}
+task2('+', 12, 11.4, 5);*/
+
+/*function task3($first, $sec)
+{
+    if (is_int($first) && is_int($sec)) {
+        echo '<table border="1px solid black">';
+        for ($i = 1; $i <= $first; $i++) {
+            echo '<tr>';
+
+            for ($j = 1; $j <= $sec; $j++) {
+                echo '<td>' . $i * $j . '</td>';
+            }
+            echo '</tr>';
+        }
+
+        echo '</table>';
+    } else {
+        echo "ERROR";
+    }
+}
+
+task3(4, 5);*/
+
+/*echo date('d.m.Y H:i').PHP_EOL;*/
+
+/*$time = mktime(0,0,0,2,24,2016);
+echo date('d.m.Y H:i', $time);*/
+
+//$string = 'Карл у Клары украл Кораллы';
+//$string2 = 'Две бутылки лимонада';
+
+/*echo str_replace('К', '', $string);*/
+
+//echo str_replace('Две', 'Три', $string2) . '<br>';
+//
+//echo str_replace('лимонада', 'воды', $string2);
+
+//$hello = 'Hello again!';
+//
+//file_put_contents('test.txt', $hello);
+//echo file_get_contents('test.txt');
+
